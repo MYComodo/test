061793df2d094b4b7313d1d0c1b5ed2dadd2a3e6
MYComodo <my956024@gmail.com>
2020-04-01 21:45:12 +0300

    Mu ham med Y ILM Az

---
+++ Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MUHAMMED on 1.04.2020.
+//
+
+#include "Test.h"
---
+++ Test.h
@@ -0,0 +1,16 @@
+//
+// Created by MUHAMMED on 1.04.2020.
+//
+
+#ifndef TESTREPO_TEST_H
+#define TESTREPO_TEST_H
+
+
+
+class Test {
+
+};
+
+
+
+#endif //TESTREPO_TEST_H
